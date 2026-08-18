3e6a5cc8dd645968c0818393172e52c096adad8a
lacsar712 <liug07115@gmail.com>
2026-08-18 21:04:34 +0800

    Update inbound JSON parse errors.

--- internal/event/event.go
+++ internal/event/event.go
@@ -46,7 +46,7 @@ func Parse(body []byte) (CoreEvent, error) {
 	}
 	var env CoreEvent
 	if err := json.Unmarshal(body, &env); err != nil {
-		return CoreEvent{}, fmt.Errorf("json: %w", err)
+		return CoreEvent{}, fmt.Errorf("json: %v", err)
 	}
 	if err := ValidateType(env.Type); err != nil {
 		return CoreEvent{}, err
