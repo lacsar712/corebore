aaa9c480899db7235ca4b5442efcef27e97ac2ee
lacsar712 <liug07115@gmail.com>
2026-08-18 21:04:28 +0800

    Update outbound request construction.

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -97,7 +97,7 @@ func (e *Engine) handle(ctx context.Context, j job.Job) {
 	}
 
 	j.Attempt++
-	res := e.client.Post(ctx, lab.Request{
+	res := e.client.Post(context.Background(), lab.Request{
 		URL:           dest.URL,
 		Secret:        dest.Secret,
 		EventID:       j.EventID,
